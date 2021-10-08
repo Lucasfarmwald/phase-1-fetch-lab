fea1facf6c405f281532765c242dea78b1e2b0cb
lucasfarmwald <farmwaldlucas@gmail.com>
2021-10-07 21:37:27 -0500

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  //fetch("https://anapioficeandfire.com/api/books")
+    //(response => response.json)
+     //then(renderBooks(response))
+   return fetch
+// To pass the tests, don't forget to return your fetch!
 }
+
+fetch("https://anapioficeandfire.com/api/books")
+    .then(response => response.json)
+     (renderBooks(response))
+
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
